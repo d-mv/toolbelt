3d464e7c921dad66b533acd5a5f264f7673baa9f
Dmitry Melnikov <dmitry.melnikov@changehealthcare.com>
2022-11-22 14:35:23 +0200

    fix: setupTxt to use section as string or array

--- src/tools/text.tools.ts
+++ src/tools/text.tools.ts
@@ -4,15 +4,17 @@ import { isNilOrEmpty } from './logic.tools';
 import { logger } from './log.tools';
 
 export function setupTxt(textObject: Record<string, unknown>) {
-  return function txt(section: string[]) {
-    const messageSection = path(section, textObject) as Record<string, string>;
+  return function txt(section: string | string[]) {
+    const sectionToUse = Array.isArray(section) ? section : [section];
+
+    const messageSection = path(sectionToUse, textObject) as Record<string, string>;
 
     if (isNilOrEmpty(messageSection)) throw new Error('Missing errors templates');
 
     return function getMessage(message: string) {
       const result = path<string>([message], messageSection);
 
-      if (!result) logger.warn(`Missing message at ${section.join('/')}/${message}`);
+      if (!result) logger.warn(`Missing message at ${sectionToUse.join('/')}/${message}`);
 
       return result ?? '';
     };
