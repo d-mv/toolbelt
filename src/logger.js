e636705e67926cf4b9324bb8ec697586871f08c8
Dmitry Melnikov <dmitry.melnikov@changehealthcare.com>
2023-09-07 09:12:12 +0100

    fix: logger had hardcoded values

--- src/logger.js
+++ src/logger.js
@@ -1,94 +1,90 @@
+/* eslint-disable no-console */
 function decodeMessage(m) {
-	if (m === null || m === undefined) return m;
+  if (m === null || m === undefined) return m;
 
-	let result = m;
+  let result = m;
 
-	if (typeof result === "string") {
-		try {
-			result = JSON.parse(result);
-		} catch (e) {}
-	}
+  if (typeof result === 'string') {
+    try {
+      result = JSON.parse(result);
+    } catch (e) {
+      // ignore
+    }
+  }
 
-	if (typeof result === "object") {
-		const r = {};
+  if (typeof result === 'object') {
+    const r = {};
 
-		for (const [key, value] of Object.entries(result)) {
-			r[key] = decodeMessage(value);
-		}
-		result = r;
-	}
+    for (const [key, value] of Object.entries(result)) {
+      r[key] = decodeMessage(value);
+    }
 
-	return result;
+    result = r;
+  }
+
+  return result;
 }
 
 const LOG = new Proxy(
-	{
-		metrics: (message) => {
-			console.groupCollapsed(
-				`%cMETRICS::${message.name}`,
-				"background-color:#cce8f9;color:#444;padding:2px 8px;border-radius: 1px;",
-			);
-			console.log(message);
-			console.groupEnd();
-		},
-		api: (message) => {
-			console.groupCollapsed(
-				`%cAPI::${message.origin}`,
-				"background-color:#cce8f9;color:#444;padding:2px 8px;border-radius: 1px;",
-			);
-			console.log(message);
-			console.groupEnd();
-		},
-		info: (message) => {
-			console.log(
-				`%cINFO::${"Invalid config data"}`,
-				"background-color:#59fe454f;color:#444;padding:2px 8px;border-radius: 1px;",
-			);
-		},
-		error: (message) => {
-			console.log(
-				`%cERROR::${
-					typeof message === "string" ? message : JSON.stringify(message)
-				}`,
-				"background-color:#e80000;color:#fff;padding:2px 8px;border-radius: 1px;",
-			);
-		},
-		state_change: ({ stateName, currVal, newVal }) => {
-			console.groupCollapsed(
-				`%cSTATE_CHANGE::${stateName}`,
-				"background-color:#243aa838;color:#243aa8;padding:2px 8px;border-radius: 1px;",
-			);
-			console.log("CURRENT:", currVal);
-			console.log("NEW:", newVal);
-			console.groupEnd();
-		},
-	},
-	{
-		get: (target, prop) => {
-			if (prop in target) return target[prop];
-			return (message) => console.log(message);
-		},
-	},
+  {
+    metrics: message => {
+      console.groupCollapsed(
+        `%cMETRICS::${message.name}`,
+        'background-color:#cce8f9;color:#444;padding:2px 8px;border-radius: 1px;',
+      );
+      console.log(message);
+      console.groupEnd();
+    },
+    api: message => {
+      console.groupCollapsed(
+        `%cAPI::${message.origin}`,
+        'background-color:#cce8f9;color:#444;padding:2px 8px;border-radius: 1px;',
+      );
+      console.log(message);
+      console.groupEnd();
+    },
+    info: message => {
+      console.log(`%cINFO::${message}`, 'background-color:#59fe454f;color:#444;padding:2px 8px;border-radius: 1px;');
+    },
+    error: message => {
+      console.log(
+        `%cERROR::${typeof message === 'string' ? message : JSON.stringify(message)}`,
+        'background-color:#e80000;color:#fff;padding:2px 8px;border-radius: 1px;',
+      );
+    },
+    state_change: ({ stateName, currVal, newVal }) => {
+      console.groupCollapsed(
+        `%cSTATE_CHANGE::${stateName}`,
+        'background-color:#243aa838;color:#243aa8;padding:2px 8px;border-radius: 1px;',
+      );
+      console.log('CURRENT:', currVal);
+      console.log('NEW:', newVal);
+      console.groupEnd();
+    },
+  },
+  {
+    get: (target, prop) => {
+      if (prop in target) return target[prop];
+
+      return message => console.log(message);
+    },
+  },
 );
 
 function logMessage(detail) {
-	const { type, message } = decodeMessage(detail);
-	LOG[type](message);
+  const { type, message } = decodeMessage(detail);
+
+  LOG[type](message);
 }
 
 function logger(enable = true) {
-	if (enable) {
-		globalThis.document.addEventListener("log", (data) =>
-			logMessage(data.detail),
-		);
-		globalThis.console.log("Logging enabled");
-	} else {
-		globalThis.document.removeEventListener("log", (data) =>
-			logMessage(data.detail),
-		);
-		// eslint-disable-next-line no-console
-		console.log("Logging disabled");
-	}
+  if (enable) {
+    globalThis.document.addEventListener('log', data => logMessage(data.detail));
+    globalThis.console.log('Logging enabled');
+  } else {
+    globalThis.document.removeEventListener('log', data => logMessage(data.detail));
+    console.log('Logging disabled');
+  }
 }
 
 globalThis.window.logger = logger;
