56530adcdff1ab198bb6a27646b440f5d0ebcbf3
Dmitry Melnikov <d.dash.mv@gmail.com>
2025-10-16 14:42:03 +0100

    chore: minor changes

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,25 +1,14 @@
-export * from "./async.tools";
-
-export * from "./file.tools";
-
-export * from "./international";
-
-export * from "./latency.tools";
-
-export * from "./log.tools";
-
-export * from "./logic.tools";
-
-export * from "./number.tools";
-
 export * from "./array.tools";
-
+export * from "./async.tools";
+export * from "./file.tools";
+export * from "./international";
+export * from "./latency.tools";
+export * from "./log.tools";
+export * from "./logic.tools";
+export * from "./number.tools";
 export * from "./object.tools";
-
 export * from "./string.tools";
-
 export * from "./text.tools";
-
 export * from "./type.tools";
-
 export * from "./validators";
+export * from './monad.tools'
